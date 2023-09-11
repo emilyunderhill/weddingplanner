dc5e089c9ca634cee9a4a7ab2f7f27dd7d382e92
Emily Underhill <emilyru93@gmail.com>
2023-09-11 19:13:06 +0100

    added modal skeleton

---
+++ frontend/src/components/Modal/index.tsx
@@ -0,0 +1,50 @@
+import { FC, ReactNode, useRef } from "react";
+import './style.scss'
+import Button from "../Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+
+type Props = {
+  header: string
+  content: ReactNode | string
+  footer: ReactNode
+  isOpen: boolean
+  onClose: () => void
+}
+
+const Modal: FC<Props> = ({isOpen, header, content, onClose, footer}) => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const handleOnClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      //Closes modal if user clicks outside
+      if (event.target === wrapperRef.current){
+        onClose()
+      }
+  }
+
+  return (
+    <div className="modal-wrapper" ref={wrapperRef} onClick={handleOnClick}>
+      <div className="modal">
+        <div className="modal-close" onClick={onClose}>
+          <FontAwesomeIcon icon={solid("xmark")} />
+        </div>
+         <div className="modal-header">
+          <p>{header}</p>
+        </div>
+        <hr />
+        <div className="modal-content">
+          {content}
+        </div>
+        <div className="modal-footer">
+          {footer}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
